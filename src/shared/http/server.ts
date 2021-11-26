c40111fd2ee7d9c51494d14322ea13df2a70be12
Antenor <antenorzapata@gmail.com>
2021-11-26 20:57:40 -0300

    feat: create docker container and config database connection

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,16 @@
+import 'reflect-metadata';
 import express from 'express';
 import cors from 'cors';
 import routes from '../routes';
+import error from '../middlewares/error';
+import '@shared/typeorm';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(error);
 
 app.listen(3333, () => {
 	console.log('Server running on port 3333!');
