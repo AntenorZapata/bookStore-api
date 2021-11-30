32935e35fa15c0d18d75eb171efc780a4362993c
Antenor <antenorzapata@gmail.com>
2021-11-29 22:03:14 -0300

    feat: create User model and repository

---
+++ src/modules/users/typeorm/repositories/UserRepository.ts
@@ -0,0 +1,10 @@
+import { EntityRepository, Repository } from 'typeorm';
+import User from '../entities/User';
+
+@EntityRepository(User)
+export default class UserRepository extends Repository<User> {
+	public async findByName(name: string): Promise<User | undefined> {
+		const user = this.findOne({ name });
+		return user;
+	}
+}
